a30990f24ef8cfdc278bc5c0056c410bb688fbce
sujal kumar <sujalkumar0444@gmail.com>
2024-06-25 14:13:57 +0530

    add route

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import ProtectedRoute from "./components/ProtectedRoute";
+import './App.css';
+import { BrowserRouter, Routes, Route, } from "react-router-dom";
 import Login from './components/Login'
 
 function App() {
   return (
-    <Login/>
+    <BrowserRouter>
+    <Routes>
+      <Route exact path="/" element={<Login />} />
+      <Route element={<ProtectedRoute />}>
+        <Route exact path="/courses" element={<>hello</>} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+
   )
   }
 
